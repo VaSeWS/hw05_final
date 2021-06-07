ab101bfe00b816d47f0393870fe7a7b6998c4988
Vasilii Safronov <vks20001@yandex.ru>
2021-06-07 17:55:10 +0300

    Fixed imports

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -3,7 +3,6 @@ import time
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
